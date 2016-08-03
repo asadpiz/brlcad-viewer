d8835732098999b226f8ed30f9e2768d483b5cf7
asadxflow@gmail.com <asadxflow@gmail.com>
2016-08-04 00:22:37 +0200

    adding db_ls() test

---
+++ test.c
@@ -0,0 +1,19 @@
+#include "common.h"
+#include "raytrace.h"
+
+
+int main(void) {
+  struct db_i *dbip = db_open("ktank.g", DB_OPEN_READONLY);
+  struct directory **tops;
+  db_dirbuild(dbip);
+  int count = db_ls(dbip, DB_LS_TOPS, NULL, &tops);
+  bu_log("found %d top level objects\n", count);
+  while (count > 0) {
+    bu_log("top path is %s\n", tops[count-1]->d_namep);
+    count--;
+  }
+  if (tops)
+    bu_free(tops, "free tops");
+  return 0;
+}
+
